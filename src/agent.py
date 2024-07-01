64acd92c99d839b473f56588027f47ceeb5c1360
Vtorrealba <102916964+Vtorrealba@users.noreply.github.com>
2024-07-01 20:43:14 +0000

    fix: api request tool

--- src/agent.py
+++ src/agent.py
@@ -1,15 +1,16 @@
 import os 
 import dotenv
+import json
 
-
-from typing_extensions import TypedDict
 from typing import Annotated
-from langchain_openai import ChatOpenAI
+from typing import Literal
+from typing_extensions import TypedDict
+from langchain_anthropic import ChatAnthropic
 from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain_core.messages import ToolMessage
 from langgraph.prebuilt import ToolNode
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
-
 
 dotenv.load_dotenv()
 
@@ -20,11 +21,33 @@ os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# 2. define tools
-# requests_post = load_tools(["requests_post"], allow_dangerous_tools=True)
-# tools = [requests_post]
-# tool_node = ToolNode(tools)
+# 2. define tools and tool node
+tool = load_tools(["requests_post"], allow_dangerous_tools=True)
+tools = [tool]
+class BasicToolNode:
+    def __init__(self, tools:list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+        
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No messages found in inputs")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content= json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
 
+tool_node = BasicToolNode(tools=[tools])
 
 # 3. graph state
 class State(TypedDict):
@@ -33,13 +56,16 @@ class State(TypedDict):
 graph_builder = StateGraph(State)
 
 # 4. define chatbot node and workflow
-llm = ChatOpenAI(temperature=0.9, model_name="gpt-4-turbo")
-
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+llm_with_tools = llm.bind_tools(tool_node)
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
 
 graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_node("tools", tool_node)
 
 graph_builder.set_entry_point("chatbot")
 
@@ -47,11 +73,3 @@ graph_builder.set_finish_point("chatbot")
 
 graph = graph_builder.compile()
 
-while True:
-    user_input = input("user:")
-    if user_input in ["exit", "quit", "q"]:
-        print("exiting...")
-        break
-    for event in graph.stream({"messages": ("user", user_input)}):
-        for value in event.values():
-            print("assistant:", value["messages"][-1].content)
