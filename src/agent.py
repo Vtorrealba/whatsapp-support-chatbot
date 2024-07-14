b2da96c3a26525e3219527df032ee96cbd53b242
Vtorrealba <102916964+Vtorrealba@users.noreply.github.com>
2024-07-14 22:58:59 +0000

    fix: streamlit history bug

--- src/agent.py
+++ src/agent.py
@@ -1,35 +1,45 @@
 import os 
 import dotenv
-import json
 import requests
-import uuid
-import streamlit as st
 
 from datetime import datetime
-from typing import Annotated
-from typing import Literal
-from typing_extensions import TypedDict
 from langchain import hub
 from langchain_anthropic import ChatAnthropic
-from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, CallbackManagerForToolRun
+from langchain_core.pydantic_v1 import BaseModel, Field, EmailStr
 from langchain_core.messages import ToolMessage
 from langchain_core.runnables import Runnable, RunnableConfig, RunnableLambda
-from langchain_core.tools import tool
+from langchain_core.tools import tool, BaseTool
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.graph import END, StateGraph, START
+from langgraph.graph import StateGraph
 from langgraph.graph.message import AnyMessage, add_messages
+from typing_extensions import TypedDict
+from typing import (
+    Annotated,
+    Type, 
+    Optional, 
+)
+from utils.agent_helpers import _tool_prompt_loader
 
-dotenv.load_dotenv()
 
 # 1. setup observability and env vars
+dotenv.load_dotenv()
 os.environ['LANGCHAIN_PROJECT'] = "Sweep chatbot"
 os.environ['LANGCHAIN_TRACING_V2'] = "true"
 os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
-# 2. define tool node and tools
+# 2. define tool node and tools, define the schemas for the tool inputs
+class CheckCalendar(BaseModel):
+    date: str = Field(..., description="The date to check availability in the calendar. input should be a future date in MM/DD/YYYY format (e.g., 11/22/2024)")
+class AppointmentBooking(BaseModel):
+    name: str = Field(description="The name of the person or company booking the appointment", min_length=2, max_length=100)
+    email: EmailStr = Field(..., description="The email of the person or company booking the appointment")
+    phone: str = Field(description="The phone number of the person or company booking the appointment. trim the plus sign if present", pattern=r'^1?\d{9,15}$')
+    date: str = Field(..., description="The date and time of the appointment in ISO 8601 format (e.g., 2024-07-27T13:00:00.000Z)")
+    reason: str = Field(description="The reason of the appointment booking", max_length=100)
 
 # tool error handling
 def handle_tool_error(state) -> dict:
@@ -51,12 +61,10 @@ def create_tool_node_with_fallback(tools:list) -> dict:
         [RunnableLambda(handle_tool_error)], exception_key="error"
     )                                           
 
-@tool("check_calendar")
-def check_calendar(date: str) -> dict:
-    """Check the availability of a date provided by the user in the calendar. 
-       the date should be in the format MM/DD/YYYY
-    """
-    webhook_url = os.getenv("WEBHOOK_URL")
+@tool("check_calendar", args_schema=CheckCalendar)
+def check_calendar(date:str) -> dict:
+    """Check the availability of a date provided by the user in the calendar. convert the date provided by the user to the mm/dd/yyyy format""" # don't ever touch this
+    webhook_url = os.getenv("CC_WEBHOOK_URL")
     response = requests.post(webhook_url, json={"start_date": date})    
     if response.status_code == 200:
         response_data = response.json()
@@ -66,12 +74,53 @@ def check_calendar(date: str) -> dict:
             return {"availability": response_data["slots"]}
     else:
         return {"error": f"{response.status_code} bad request"}
-        
-tools = [check_calendar]
+
+check_calendar_prompt = hub.pull("check_calendar_tool")
+check_calendar.description = _tool_prompt_loader(check_calendar_prompt)
+
+
+book_appointment_prompt = hub.pull("book_appointment_tool")
+book_appointment_tool_description = _tool_prompt_loader(book_appointment_prompt)
+class BookAppointmentTool(BaseTool):
+    name = "book_appointment"
+    description = book_appointment_tool_description
+    args_schema: Type[BaseModel] = AppointmentBooking
+
+    def _run(
+        self, name: str, email: str, phone: str, date: str, reason: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Book an appointment."""
+        webhook_url = os.getenv("PCP_WEBHOOK_URL")
+        payload = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "date": date,
+            "reason": reason
+        }
+        response = requests.post(webhook_url, json=payload)
+        if response.status_code == 200:
+            return "Appointment successfully booked."
+        else:
+            return f"Failed to book appointment. Status code: {response.status_code}"
+
+    async def _arun(
+        self, name: str, email: str, phone: str, date: str, reason: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Asynchronously book an appointment."""
+        # Implement async version if needed
+        raise NotImplementedError("Async version not implemented")
+
+# Create an instance of the tool
+book_appointment = BookAppointmentTool()
+
+
+tools = [check_calendar, book_appointment]
+
 
 # 3. define state
 class State(TypedDict):
-    messages: Annotated[list, add_messages]
+    messages: Annotated[list[AnyMessage], add_messages]
     
 
 # 4. assistant node
@@ -96,7 +145,7 @@ class Assistant:
                 state = {**state, "messages": messages}
             else:
                 break
-        return {"messages": [result]}
+        return {"messages": result}
             
     
     
@@ -105,9 +154,7 @@ llm = ChatAnthropic(model="claude-3-5-sonnet-20240620", temperature=1)
 
 #5. bind tools node to llm
 primary_assistant_prompt = hub.pull("customer_support_chatbot").partial(time=datetime.now())
-
 part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(tools)
-
 
 # 6. define graph workflow
 builder = StateGraph(State)
@@ -130,58 +177,28 @@ builder.add_edge("tools", "assistant")
 # this is a complete memory for the entire graph.
 memory = SqliteSaver.from_conn_string(":memory:")
 part_1_graph = builder.compile(checkpointer=memory)
-thread_id = str(uuid.uuid4())
+thread_id = "1"
 
 config = {
     "configurable": {
         "user_id": "21458856",
-        # Checkpoints are accessed by thread_id
         "thread_id": thread_id,
     }
-} 
+}
 
 
-# 7. streamlit interface
-st.set_page_config(page_title="Sweep 🏠",initial_sidebar_state="collapsed")
-
-st.markdown(
-    """
-    <style>
-        div#root{
-            font-family: Arial, sans-serif;
-            background: white;
-            color: black;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.title("🏠 Sweep Home Services")
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Function to handle user input
-def handle_input():
-    user_input = st.session_state.input_text
-    st.session_state.input_text = ""
-    if user_input.lower() in ["quit", "exit", "q"]:
-        st.session_state.messages.append({"role": "assistant", "content": "Goodbye!"})
+while True:
+    user_input = input("How can we help 👨‍🔧?: ")
+    if user_input.lower() in ["q","quit","exit"]:
+        print("assistant: Goodbye!")
+        break
     else:
-        st.session_state.messages.append({"role": "user", "content": user_input})
         for event in part_1_graph.stream({"messages": ("user", user_input)}, config):
             for value in event.values():
-                st.session_state.messages.append({"role": "assistant", "content": value["messages"][0].content})
+                try:
+                    print(f"assistant: {value['messages'].content}")
+                except:
+                    print(f"tool: {value['messages'][0].content}")
 
-# User input text box
-st.text_input("How can we help 👨‍🔧? ", key="input_text", on_change=handle_input)
 
-# Display chat history
-for message in st.session_state.messages:
-    if message["role"] == "user":
-        st.markdown(f"**User:** {message['content']}")
-    else:
-        st.markdown(f"**Assistant:** {message['content']}")
 
